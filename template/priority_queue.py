3e93b98e876ba19bd27f35b0b3542841ed68c56e
pluuus <11610127@gmail.com>
2019-04-03 00:19:53 +0800

    Change class name

--- template/priority_queue.py
+++ template/priority_queue.py
@@ -3,7 +3,7 @@ from functools import total_ordering
 
 
 @total_ordering
-class my_class:
+class MyClass:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -20,7 +20,7 @@ class my_class:
 
 q = queue.PriorityQueue()
 
-a = [my_class(1, 2), my_class(1, 3), my_class(2, 0)]
+a = [MyClass(1, 2), MyClass(1, 3), MyClass(2, 0)]
 
 for c in a:
     q.put(c)
