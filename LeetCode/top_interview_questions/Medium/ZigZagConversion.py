f60a9a3aabdb8cfa750b6af5bff771e25fb06e92
zhenghao <zhenghaosustc@gmail.com>
2019-04-12 20:43:53 +0800

    Change a new medium question.

--- LeetCode/top_interview_questions/Medium/ZigZagConversion.py
+++ LeetCode/top_interview_questions/Medium/ZigZagConversion.py
@@ -8,7 +8,7 @@ class Solution:
             second_match = l / 2 - r
             line = [x for i, x in enumerate(s) if (i + first_match) % l == 0 or (i + second_match) % l == 0]
             result.append(''.join(line))
-        result.append(''.join([x for i, x in enumerate(s) if (i + l) %  == 0]))
+        result.append(''.join([x for i, x in enumerate(s) if (i + l / 2) % l == 0]))
 
         return '\n'.join(result)
 
