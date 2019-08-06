fa8558ccdc24aa2d2c620a66762df8bb7d356e22
zh-plus <11610127@mail.sustech.edu.cn>
2019-08-06 15:18:30 +0800

    Tree Traversal: iterative LDR done

--- template/tree_traversal.py
+++ template/tree_traversal.py
@@ -1,4 +1,4 @@
-from queue import LifoQueue
+from queue import LifoQueue, Queue
 
 
 class TreeNode:
@@ -7,6 +7,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    @staticmethod
+    def from_array(array):
+        # bfs construct binary tree
+        root = TreeNode(array[0])
+        q = Queue()
+        q.put(root)
+
+        i = 1
+        while not q.empty() and i != len(array):
+            node = q.get()
+            node.left = TreeNode(array[i])
+            q.put(node.left)
+
+            if i + 1 != len(array):
+                node.right = TreeNode(array[i + 1])
+                q.put(node.right)
+
+            i += 2
+
+        return root
+
 
 def ldr(root: TreeNode):
     stack = LifoQueue()
@@ -14,10 +35,16 @@ def ldr(root: TreeNode):
 
     while node or not stack.empty():
         # go to the most left node
-        while node.left:
+        while node:
             stack.put(node)
             node = node.left
 
-        print(node.val)
+        node = stack.get()
+        print(node.val, end=' ')
         node = node.right
 
+
+if __name__ == '__main__':
+    a = [3, 1, 5, 0, None, 4, 8]
+    root = TreeNode.from_array(a)
+    ldr(root)
