45bef6bb31f45b5fee07323cf21ddbfe5ac51d2e
zhenghao <zhenghaosustc@gmail.com>
2019-08-07 23:43:40 +0800

    Done Newton' Method Algorithm

---
+++ LeetCode/top_interview_questions/Easy/Sqrt.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(self._sqrt(x))
+
+    def _sqrt(self, x):
+        xn = 6
+        for _ in range(5):
+            xn = xn - ((xn ** 2 - x) / (2 * xn))
+
+        return xn
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s._sqrt(1000)
+    print(r)
+
+    print(r ** 2)
